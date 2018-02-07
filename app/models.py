f5647aa35fa72738e6254f8a63f11445dc757fa4
munenelewis <munene2379@gmail.com>
2018-02-07 15:11:18 +0300

    created table admin

--- app/models.py
+++ app/models.py
@@ -40,3 +40,21 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic") 
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    id=db.Column(db.Integer,primary_key = True)
+    name=db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
