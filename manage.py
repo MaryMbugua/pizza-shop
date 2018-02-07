9dab5dc54083ef0bfe65e3f45a0272220f19e260
munenelewis <munene2379@gmail.com>
2018-02-07 15:11:05 +0300

    added user and  and role

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from app.models import User
+from app.models import User,Role
 from  flask_migrate import Migrate, MigrateCommand
 
 
@@ -17,7 +17,7 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db)
+    return dict(app = app,db = db,Role=Role,User=User)
 
 if __name__ == '__main__':
     manager.run()
